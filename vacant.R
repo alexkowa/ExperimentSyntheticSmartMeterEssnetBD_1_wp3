e58eaffa385fea5972dc54a4df95f879e39cbf6d
Johannes Gussenbauer - QM <gussenbauer@osrp01.statistik.local>
2017-01-27 12:22:49 +0100

    Update v3 2017-01-27

--- vacant.R
+++ vacant.R
@@ -1,27 +1,30 @@
-library(robustbase);library(data.table);library(forecast)
+library(robustbase)
+library(data.table)
+library(forecast)
 load("hhWithCustomerKey.RData") 
+
 sampKey <- hy[!duplicated(CUSTOMER_KEY)][1:10,CUSTOMER_KEY]
-smr <- fread("/hdfs/datasets/smartmeters/metering_data/metering_data.csv")
-smr <- smr[CUSTOMER_ID%in%cust[,CUSTOMER_KEY],]
-setnames(smr,"CUSTOMER_ID","CUSTOMER_KEY")
-setkey(smr,CUSTOMER_KEY)
-#Remove unnecessary vars
-smr[,c("EVENT_KEY","CALENDAR_KEY","CONTROLLED_LOAD_KWH","GROSS_GENERATION_KWH",
-       "NET_GENERATION_KWH","OTHER_KWH"):=NULL]
-#only keep 1 year (2013)
-smr <- smr[year(READING_DATETIME)==2013]
+rm(hy)
+
+load("/mnt/meth/BIGDATA/Smartmeter/smr.RData")
+smr[,YEAR:=substr(READING_DATETIME,1,4)]
+smr <- smr[YEAR==2013,.(CUSTOMER_KEY,READING_DATETIME,GENERAL_SUPPLY_KWH)]
+smr[,DAY:=substr(READING_DATETIME,6,10)]
+
+#######################################################################
+# EXAMPLES
 smr1 <- smr[CUSTOMER_KEY%in%sampKey]
-smr1[,date:=as.Date(READING_DATETIME)]
-smr1[CUSTOMER_KEY==10640431,plot(GENERAL_SUPPLY_KWH~as.Date(READING_DATETIME),type="l")]
-smr1[CUSTOMER_KEY==10640431&as.Date(READING_DATETIME)=="2013-01-01",summary(GENERAL_SUPPLY_KWH)]
-smr1[CUSTOMER_KEY==10640431,summary(READING_DATETIME)]
+smr1[,DAY:=as.Date(DAY)]
+smr1[CUSTOMER_KEY==11162831,plot(GENERAL_SUPPLY_KWH~as.Date(DAY),type="l")]
+smr1[CUSTOMER_KEY==11162831&as.Date(DAY)=="2013-01-01",summary(GENERAL_SUPPLY_KWH)]
+smr1[CUSTOMER_KEY==11162831,summary(DAY)]
 x <- cbind(
   rep(seq(-pi,0,l=3),16),
-#rep(seq(-pi,0,l=4),12),
-#rep(seq(-pi,0,l=6),8),
-rep(seq(-pi,0,l=8),6),
-#rep(seq(-pi,0,l=12),4),
-rep(seq(-pi,0,l=16),3)
+  #rep(seq(-pi,0,l=4),12),
+  #rep(seq(-pi,0,l=6),8),
+  rep(seq(-pi,0,l=8),6),
+  #rep(seq(-pi,0,l=12),4),
+  rep(seq(-pi,0,l=16),3)
 )
 reg <- abs(sin(x))
 vacant <- function(kwh,thr,ret="pred"){
@@ -61,17 +64,17 @@ vacant1 <- function(kwh,ret="x"){
 }
 
 
-smr1[,nobs:=.N,by=.(CUSTOMER_KEY,day)]
+smr1[,nobs:=.N,by=.(CUSTOMER_KEY,DAY)]
 smr1 <- smr1[nobs==48]
-smr1[,out:=vacant(GENERAL_SUPPLY_KWH,.5),by=.(CUSTOMER_KEY,day)]
-smr1[,outvol:=sd(diff(log(GENERAL_SUPPLY_KWH))),by=.(CUSTOMER_KEY,day)]
-smr1[,outts:=vacant1(GENERAL_SUPPLY_KWH,ret="pred"),by=.(CUSTOMER_KEY,day)]
-v1 <- smr1[,.(out=vacant(GENERAL_SUPPLY_KWH,.5,ret="out")),by=.(CUSTOMER_KEY,day)]
-v2 <- smr1[,.(outts=vacant1(GENERAL_SUPPLY_KWH)),by=.(CUSTOMER_KEY,day)]
-v3 <- smr1[,.(outvol=sd(diff(scale(GENERAL_SUPPLY_KWH)))),by=.(CUSTOMER_KEY,day)]
-v4 <- smr1[,.(outvar=var(GENERAL_SUPPLY_KWH)),by=.(CUSTOMER_KEY,day)]
-smr1[,daytime:=!hour(date)%in%c(0:7,20:23)]
-v5 <- smr1[,.(outm=mean(GENERAL_SUPPLY_KWH[daytime])/mean(GENERAL_SUPPLY_KWH[!daytime])),by=.(CUSTOMER_KEY,day)]
+smr1[,out:=vacant(GENERAL_SUPPLY_KWH,.5),by=.(CUSTOMER_KEY,DAY)]
+smr1[,outvol:=sd(diff(log(GENERAL_SUPPLY_KWH))),by=.(CUSTOMER_KEY,DAY)]
+smr1[,outts:=vacant1(GENERAL_SUPPLY_KWH,ret="pred"),by=.(CUSTOMER_KEY,DAY)]
+v1 <- smr1[,.(out=vacant(GENERAL_SUPPLY_KWH,.5,ret="out")),by=.(CUSTOMER_KEY,DAY)]
+v2 <- smr1[,.(outts=vacant1(GENERAL_SUPPLY_KWH)),by=.(CUSTOMER_KEY,DAY)]
+v3 <- smr1[,.(outvol=sd(diff(scale(GENERAL_SUPPLY_KWH)))),by=.(CUSTOMER_KEY,DAY)]
+v4 <- smr1[,.(outvar=var(GENERAL_SUPPLY_KWH)),by=.(CUSTOMER_KEY,DAY)]
+smr1[,daytime:=!HOUR%in%c(0:7,20:23)]
+v5 <- smr1[,.(outm=mean(GENERAL_SUPPLY_KWH[daytime])/mean(GENERAL_SUPPLY_KWH[!daytime])),by=.(CUSTOMER_KEY,DAY)]
 par(ask=TRUE)
 plotf <- function(x,y,z,m){
   plot(x~z,type="l",main=paste0("Correlation:", m),xlab="hour",ylab="kwh")
@@ -87,14 +90,14 @@ plot((mean(x)*reg[,6])~zz,type="l",col="blue")
 ind <- which(abs(v2$outts)>.9)[1:10]
 ind <- sample(1:nrow(v2),10)
 for(i in 1:10){
-  smr1[CUSTOMER_KEY==v2[ind[i],CUSTOMER_KEY]&day==v2[ind[i],day],plotf(GENERAL_SUPPLY_KWH,outts,date,round(v2[ind[i],outts],2))]  
+  smr1[CUSTOMER_KEY==v2[ind[i],CUSTOMER_KEY]&DAY==v2[ind[i],DAY],plotf(GENERAL_SUPPLY_KWH,outts,HOUR,round(v2[ind[i],outts],2))]  
 }
 
 summary(v1$out)
 setkey(v1,out)
 ind <- head(1:nrow(v1),10)
 for(i in 1:10){
-  smr1[CUSTOMER_KEY==v1[ind[i],CUSTOMER_KEY]&day==v1[ind[i],day],plotf(GENERAL_SUPPLY_KWH,out,date,round(v1[ind[i],out],2))]  
+  smr1[CUSTOMER_KEY==v1[ind[i],CUSTOMER_KEY]&DAY==v1[ind[i],DAY],plotf(GENERAL_SUPPLY_KWH,out,HOUR,round(v1[ind[i],out],2))]  
 }
 
 summary(v3$outvol)
@@ -102,14 +105,14 @@ v3 <- v3[!is.na(outvol)]
 setkey(v3,outvol)
 ind <- tail(1:nrow(v3),20)
 for(i in 1:10){
-  smr1[CUSTOMER_KEY==v3[ind[i],CUSTOMER_KEY]&day==v3[ind[i],day],plot(GENERAL_SUPPLY_KWH~date,main=round(v3[ind[4],outvol],2),type="l")]  
+  smr1[CUSTOMER_KEY==v3[ind[i],CUSTOMER_KEY]&DAY==v3[ind[i],DAY],plot(GENERAL_SUPPLY_KWH~HOUR,main=round(v3[ind[4],outvol],2),type="l")]  
 }
 quantile(v4$outvar,c(.05,.95),na.rm=TRUE)
 v4 <- v4[!is.na(outvar)]
 setkey(v4,outvar)
 ind <- head(1:nrow(v4),20)
 for(i in 1:10){
-  smr1[CUSTOMER_KEY==v4[ind[i],CUSTOMER_KEY]&day==v4[ind[i],day],plot(GENERAL_SUPPLY_KWH~date,main=round(v4[ind[4],outvar],2),type="l")]  
+  smr1[CUSTOMER_KEY==v4[ind[i],CUSTOMER_KEY]&DAY==v4[ind[i],DAY],plot(GENERAL_SUPPLY_KWH~HOUR,main=round(v4[ind[4],outvar],2),type="l")]  
 }
 quantile(v5$outm,c(.05,.95),na.rm=TRUE)
 v5[,outma:=abs(outm-1)]
@@ -117,8 +120,19 @@ v5 <- v5[!is.na(outma)]
 setkey(v5,outma)
 ind <- tail(1:nrow(v5),20)
 for(i in 1:10){
-  smr1[CUSTOMER_KEY==v5[ind[i],CUSTOMER_KEY]&day==v5[ind[i],day],plot(GENERAL_SUPPLY_KWH~date,main=round(v5[ind[4],outma],2),type="l")]  
+  smr1[CUSTOMER_KEY==v5[ind[i],CUSTOMER_KEY]&DAY==v5[ind[i],DAY],plot(GENERAL_SUPPLY_KWH~HOUR,main=round(v5[ind[4],outma],2),type="l")]  
 }
 plot(reg[,1]~zz,type="l",xlab="hour",ylab="Regressor",main="High frequency")
 plot(reg[,2]~zz,type="l",xlab="hour",ylab="Regressor",main="Medium frequency")
 plot(reg[,3]~zz,type="l",xlab="hour",ylab="Regressor",main="Low frequency")
+
+#######################################################################
+#######################################################################
+# Use variance to determine if household is vacant
+
+smr[,VAR.DAY:=var(GENERAL_SUPPLY_KWH),by=list(CUSTOMER_KEY,DAY)]
+smr
+
+
+
+
